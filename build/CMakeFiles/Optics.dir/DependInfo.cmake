9caf01395a9533b6a351598a5739477a60e3c8d4
MiraiiF <mirailtonfo@gmail.com>
2023-06-02 15:07:48 -0300

    Commit inicial com o setup de glfw, glm e glad.

---
+++ build/CMakeFiles/Optics.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mirai/Documentos/Optics/glad/src/glad.c" "CMakeFiles/Optics.dir/glad/src/glad.c.o" "gcc" "CMakeFiles/Optics.dir/glad/src/glad.c.o.d"
+  "/home/mirai/Documentos/Optics/main.cpp" "CMakeFiles/Optics.dir/main.cpp.o" "gcc" "CMakeFiles/Optics.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/mirai/Documentos/Optics/build/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Optics.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Optics.dir/glad/src/glad.c.o"
+  "CMakeFiles/Optics.dir/glad/src/glad.c.o.d"
+  "CMakeFiles/Optics.dir/main.cpp.o"
+  "CMakeFiles/Optics.dir/main.cpp.o.d"
+  "Optics"
+  "Optics.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Optics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
